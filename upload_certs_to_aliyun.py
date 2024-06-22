c0a5f021c35332003ebdba950d7359f53516fde3
Eureka <fengyuan-liang@foxmail.com>
2024-06-22 23:16:23 +0800

    update py script & action

--- upload_certs_to_aliyun.py
+++ upload_certs_to_aliyun.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkcdn.request.v20180510 import SetCdnDomainSSLCertificateRequest
@@ -29,7 +30,7 @@ def upload_certificate(client, domain_name, cert_path, key_path):
     # CDN加速域名
     request.set_DomainName(domain_name)
     # 证书名称
-    request.set_CertName(domain_name)
+    request.set_CertName(domain_name + datetime.datetime.now().strftime("%Y%m%d"))
     request.set_CertType('upload')
     request.set_SSLProtocol('on')
     request.set_SSLPub(cert)
